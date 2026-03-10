a560e946ff6972f2bf430b46b473712ffd2c6e25
Rafael J. Ramirez J. <153031153+rafael030124@users.noreply.github.com>
2026-03-10 12:49:06 -0400

    module4

---
+++ module4solution/SayGoodbye.js
@@ -0,0 +1,8 @@
+/**
+ * Library: sayGoodbye.js
+ * Provides the sayGoodbye function.
+ * FIX: The closing brace was missing, causing a syntax error.
+ */
+function sayGoodbye(name) {
+    console.log("Goodbye " + name);
+}
---
+++ module4solution/Sayhello.js
@@ -0,0 +1,8 @@
+/**
+ * Library: sayHello.js
+ * Provides the sayHello function.
+ * FIX: The function name was misspelled. Renamed from 'sayHllo' to 'sayHello'.
+ */
+function sayHello(name) {
+    console.log("Hello " + name);
+}
